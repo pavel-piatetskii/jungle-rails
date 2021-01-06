c05c02fd6a3b568153d7850ed5cf464d0cd5f762
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-01-06 14:43:42 -0800

    Add HTTP Basic Auth to the Admin section

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+
+  name = ENV['HTTP_AUTH_USERNAME']
+  password = ENV['HTTP_AUTH_PASSWORD']
+  http_basic_authenticate_with name: name, password: password
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,8 @@
 class Admin::ProductsController < ApplicationController
+  
+  name = ENV['HTTP_AUTH_USERNAME']
+  password = ENV['HTTP_AUTH_PASSWORD']
+  http_basic_authenticate_with name: name, password: password
 
   def index
     @products = Product.order(id: :desc).all
