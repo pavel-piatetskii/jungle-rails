6b748eedcf170f192dfc9979a59ace18750f8ce1
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2021-01-11 17:42:05 -0800

    Complete price, quantity and category validation tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
 
+    # Valid save operation
     it 'saves a product with all 4 fields filled' do
       @category = Category.new({name: 'test'})
       @category.save
@@ -18,6 +19,7 @@ RSpec.describe Product, type: :model do
       expect(@product.save).to be(true)
     end
 
+    #No name
     it 'fails to save a product without name showing related error ' do
       @category = Category.new({name: 'test'})
       @category.save
@@ -29,8 +31,53 @@ RSpec.describe Product, type: :model do
         category_id: Category.find_by_name('test').id
       })
       @product.save
-      expect(@product.errors.full_messages).to eq(["Name can't be blank"])
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
+
+    #No price
+    it 'fails to save a product without price showing related error ' do
+      @category = Category.new({name: 'test'})
+      @category.save
+
+      @product = Product.new({
+        name: 'test_name',
+        price: nil,
+        quantity: 7,
+        category_id: Category.find_by_name('test').id
+      })
+      @product.save
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end   
+
+    #No quantity
+    it 'fails to save a product without quantity showing related error ' do
+      @category = Category.new({name: 'test'})
+      @category.save
+
+      @product = Product.new({
+        name: 'test_name',
+        price: 42,
+        quantity: nil,
+        category_id: Category.find_by_name('test').id
+      })
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end  
+
+    #No category
+    it 'fails to save a product without category showing related error ' do
+      @category = Category.new({name: 'test'})
+      @category.save
+
+      @product = Product.new({
+        name: 'test_name',
+        price: 42,
+        quantity: 7,
+        category_id: nil
+      })
+      @product.save
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end  
 
   end
 
